d02fea30bd50c3d5c10272e9df79e7a8bccc2e40
agilusinformatica <claudio@agilus.com.br>
2025-03-31 16:16:54 -0300

    Primeiro commit

---
+++ start.py
@@ -0,0 +1,28 @@
+class Pessoa:
+    nome = None
+    idade = None
+    altura = None
+    __saldo = 0
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        if idade < 18:
+            raise ValueError('Idade inválida')
+        self.idade = idade
+
+    def deposito(self, valor):
+        self.__saldo += valor
+    def saque(self, valor):
+        if valor > self.__saldo:
+            raise ValueError('Saldo insuficiente')
+        self.__saldo -= valor
+
+    def saldo(self):
+        return self.__saldo
+
+
+cliente = Pessoa('Felippe', 39)
+cliente.deposito(1000)
+cliente.saque(1500)
+
+print(cliente.saldo())
