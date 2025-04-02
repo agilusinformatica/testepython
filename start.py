6af49a1bac5f50e853274f3bfb225d4ffa1369d2
agilusinformatica <claudio@agilus.com.br>
2025-04-02 15:16:53 -0300

    finalizei implementação de limite

--- start.py
+++ start.py
@@ -2,6 +2,7 @@ class Pessoa:
     nome = None
     idade = None
     altura = None
+    limite = 0
     __saldo = 0
 
     def __init__(self, nome, idade):
@@ -13,16 +14,19 @@ class Pessoa:
     def deposito(self, valor):
         self.__saldo += valor
     def saque(self, valor):
-        if valor > self.__saldo:
+        if valor > self.__saldo + self.limite:
             raise ValueError('Saldo insuficiente, seu pobre e lascado')
         self.__saldo -= valor
+    def set_limite(self, limite):
+        self.limite = limite
 
     def saldo(self):
         return self.__saldo
 
 
 cliente = Pessoa('Felippe', 39)
+cliente.set_limite(1000)
 cliente.deposito(1000)
-cliente.saque(800)
+cliente.saque(2500)
 
 print(cliente.saldo())
