38e617fe1dea347798302d4dae07e9a6b20c251c
agilusinformatica <claudio@agilus.com.br>
2025-04-02 15:02:43 -0300

    mensagem melhor ainda

--- start.py
+++ start.py
@@ -14,7 +14,7 @@ class Pessoa:
         self.__saldo += valor
     def saque(self, valor):
         if valor > self.__saldo:
-            raise ValueError('Saldo insuficiente, seu pobre')
+            raise ValueError('Saldo insuficiente, seu pobre e lascado')
         self.__saldo -= valor
 
     def saldo(self):
